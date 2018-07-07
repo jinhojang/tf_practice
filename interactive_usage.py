8772707dbd04fa0955e0352d55f0d6fcd2fc96a6
Jinho Jang <jinho@alum.mit.edu>
2018-07-07 12:11:06 -0400

    updated deprecated ones

--- interactive_usage.py
+++ interactive_usage.py
@@ -9,7 +9,7 @@ a = tf.constant([3.0, 3.0])
 x.initializer.run()
 
 # Add an op to subtract 'a' from 'x'.  Run it and print the result
-sub = tf.sub(x, a)
+sub = tf.subtract(x, a)
 print(sub.eval())
 # ==> [-2. -1.]
 
